0dcc90c4e1dac3d8d569ccb289885a32ccf22a8d
debasishrowlo <debasishrowlo@gmail.com>
2023-08-27 10:33:19 +0530

    Create firstNonConsecutive.ts

---
+++ 8kyu/firstNonConsecutive.ts
@@ -0,0 +1,9 @@
+export function firstNonConsecutive (arr: number[]) : null | number {
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] + 1 !== arr[i + 1]) {
+      return arr[i + 1]
+    } 
+  }
+  
+  return null
+}
